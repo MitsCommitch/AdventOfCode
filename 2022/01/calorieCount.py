2cd161be771efd7ad8e67ae1dd25d1461f19aefa
MitchStreams <mitscommitch@gmail.com>
2022-12-05 21:47:23 -0500

    first commit

---
+++ 2022/01/calorieCount.py
@@ -0,0 +1,16 @@
+import os
+
+def main():
+    elves = {}
+    curr = 0
+
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    cals = open(f'{path}\\calories.txt').read().splitlines()
+
+    elves = 
+
+    print(cals)
+
+if __name__ == "__main__":
+    main()
