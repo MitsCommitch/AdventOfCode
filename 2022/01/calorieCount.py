fdbb9b9eaa5a382743523ed9b032d62d1b04c0e2
MitsCommitch <mitscommitch@gmail.com>
2022-12-06 22:15:39 -0500

    Day one and day two
    Signed-off-by: MitsCommitch <mitscommitch@gmail.com>

--- 2022/01/calorieCount.py
+++ 2022/01/calorieCount.py
@@ -1,16 +1,37 @@
 import os
+import itertools
 
 def main():
-    elves = {}
-    curr = 0
 
     path = os.path.dirname(os.path.realpath(__file__))
 
-    cals = open(f'{path}\\calories.txt').read().splitlines()
+    with open(f'{path}{os.sep}calories.txt') as c:
+        calList = [int(x) if x != '' else '' for x in c.read().splitlines()]
 
-    elves = 
+    elfCalDict = processElfCalList(calList)
 
-    print(cals)
+    max_cals = getMaxN(elfCalDict, 3)
+
+    print(f'Elf with the max cals has {max_cals[0]} while the top 3 have {sum(max_cals)} total.')
+
+
+def processElfCalList(calList):
+    elves = {}
+
+    cals =  [sum(list(y)) for x, y in itertools.groupby(calList, lambda z: z == '') if not x]
+
+    elves = {i:e for i, e in zip(range(len(cals)), cals)}
+
+    return elves
+
+def getMaxN(d, n):
+    maxes = []
+
+    while len(maxes) < n:
+        m = (max(d, key=d.get))
+        maxes.append(d.pop(m))
+
+    return maxes
 
 if __name__ == "__main__":
     main()
