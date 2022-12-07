e2a5ec6b8199a092c43441802c2d381ff3c0bea4
MitsCommitch <mitscommitch@gmail.com>
2022-12-06 23:03:57 -0500

    Formatting, clarity, and documentation for day 1
    Signed-off-by: MitsCommitch <mitscommitch@gmail.com>

--- 2022/01/calorieCount.py
+++ 2022/01/calorieCount.py
@@ -2,24 +2,37 @@ import os
 import itertools
 
 def main():
-
+    """Setup and output."""
     path = os.path.dirname(os.path.realpath(__file__))
 
     with open(f'{path}{os.sep}calories.txt') as c:
+        # Input is a list of calories carried by each elf, elves 
+        # separated by blank lines
+
+        # The `if X != '' else ''` bit ensures that we convert
+        # string input to ints but preserve blank lines
         calList = [int(x) if x != '' else '' for x in c.read().splitlines()]
 
+    # Process input into a list of calories carried by each elf
     elfCalDict = processElfCalList(calList)
 
+    # Get sorted descending list of max cals
     max_cals = getMaxN(elfCalDict, 3)
 
-    print(f'Elf with the max cals has {max_cals[0]} while the top 3 have {sum(max_cals)} total.')
+    print(f'Elf {max_cals[0][0]} has the max cals with {max_cals[0][1]}')
+    print(f'The top {len(max_cals)} elves, {[i for i, j in max_cals]} have {sum([j for i, j in max_cals])} total.')
 
 
 def processElfCalList(calList):
+    """Take a blank line separated list of calories by elf and return a list
+    of each elves total calories. """
     elves = {}
 
+    # Generate a sublist for each elf, splitting groups by ''
+    # then sum the sublists
     cals =  [sum(list(y)) for x, y in itertools.groupby(calList, lambda z: z == '') if not x]
 
+    # Problem didn't state a need to know which elf has what, but just in case
     elves = {i:e for i, e in zip(range(len(cals)), cals)}
 
     return elves
@@ -29,7 +42,7 @@ def getMaxN(d, n):
 
     while len(maxes) < n:
         m = (max(d, key=d.get))
-        maxes.append(d.pop(m))
+        maxes.append((m, d.pop(m)))
 
     return maxes
 
