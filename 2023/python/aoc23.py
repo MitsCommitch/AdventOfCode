d3847eb5f5a2065fc88a98d8e98b38c0e7411ba9
MitsCommitch <mitscommitch@gmail.com>
2023-12-05 20:57:41 -0500

    A different setup and new day 1

--- 2023/python/aoc23.py
+++ 2023/python/aoc23.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import datetime as dt
+import os
 import re
 import sys
 
@@ -8,84 +10,21 @@ def init_arg_parse():
 
     return parser
 
-def get_calib_from_line(line):
-    nums = [d for d in line if d.isdigit()]
-    calib = int(f'{nums[0]}{nums[-1]}')
-
-    return calib
-
-def get_nums(line):
-    nlt = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
-    res = []
-    for num in nlt.keys():
-        hits = re.finditer(num, line)
-        for hit in hits:
-            res.append((nlt[num], hit.start()))
-    if res:
-        return  [min(res, key=lambda x:x[1]), max(res, key=lambda x:x[1])]
-
-def get_real_calib_from_line(line):
-    digits = list(filter(str.isdigit, line))
-    if digits:
-        first, last = (digits[0], digits[-1])
-        fi, li = (line.find(first), line.rfind(last))
-    wordnums = get_nums(line)
-    if wordnums:
-        fw, fwi = wordnums[0]
-        if fi > fwi:
-            first = fw
-
-        lw, lwi = wordnums[1]
-        if li < lwi:
-            last = lw
-
-    calib = int(f'{first}{last}')
-
-    return calib
-
-def day01(input):
-    sum = 0
-    real_sum = 0
-
-    for line in input:
-        sum += get_calib_from_line(line)
-        real_sum += get_real_calib_from_line(line)
-
-    print(f'Day 1 first result: {sum}')
-    print(f'Day 1 second result: {real_sum}')
-
-
-
-
-
-
-
-
-def day02(input):
-    return
-
 def main(argv):
     parser = init_arg_parse()
     args = parser.parse_args()
 
-    with open(f'./{args.day}/input.txt') as f:
-        input = f.readlines()
+    with open(f'./day_{args.day}/input.txt') as f:
+        input = f.read().splitlines()
 
     match args.day:
         case '01':
-            day01(input)
+            from day_01 import day01
+            day01.main(input)
+
         case '02':
-            day02(input)
+            from day_02 import day02
+            day02.main(input)
         case _:
             return
         
